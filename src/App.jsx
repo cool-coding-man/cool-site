672a5f0f34076d1ab1335cd676926c45e726c9df
swc9803 <swc9803@gmail.com>
2024-03-26 23:24:31 +0900

    scrollTrigger test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Header from "./components/header/Header.jsx";
 import Home from "./pages/home/index.jsx";
 import Fade from "./pages/fade/index.jsx";
+import Scroll from "./pages/scroll/index.jsx";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/fade" element={<Fade />} />
+        <Route path="/scroll" element={<Scroll />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/scroll/index.jsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+const App = () => {
+  return (
+    <div className="container">
+      <section>scroll</section>
+    </div>
+  );
+};
+
+export default App;
